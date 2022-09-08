030c74af66aacf3281af1ccb52e099ff2bdd3e9e
vince-exe <vincenzocaliendoantonio@gmail.com>
2022-09-08 02:13:07 +0200

    update
    updated the MultiChatHelper, it will be used to test the MultiChat application

---
+++ MultiChatHelper/HelperMultiChat/Source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include <boost/asio.hpp>
+
+std::string read_until(boost::asio::ip::tcp::socket* socket, const std::string& del) {
+	boost::asio::streambuf buff;
+	std::string message;
+	boost::system::error_code er;
+
+	boost::asio::read_until(*socket, buff, del, er);
+	message = boost::asio::buffer_cast<const char*>(buff.data());
+
+	return message;
+}
+
+void send(boost::asio::ip::tcp::socket* socket, const std::string& msg) {
+	boost::system::error_code er;
+	boost::asio::write(*socket, boost::asio::buffer(msg), er);
+}
+
+int main() {
+	boost::asio::io_context context;
+
+	boost::asio::ip::tcp::socket socket(context);
+
+	socket.connect(boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 8000));
+	send(&socket, "ciao\n");
+
+	return 0;
+}
